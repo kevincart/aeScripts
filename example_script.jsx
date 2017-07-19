ef2641831727ebff4afc8710df3beb4e648bcb7c
Kevin Cartwright <kevincart@gmail.com>
2017-07-19 17:39:04 +1200

    initial commit, Adobe Aftereffects Scripts by Kevin Cartwright

---
+++ example_script.jsx
@@ -0,0 +1,9 @@
+﻿var message = "You just sent a variable to an alert";
+var err = function err(errString) {
+    alert(errString);
+    }
+app.onError = err;
+
+alert(message);
+
+app.newProject();
---
+++ myScript1.jsx
@@ -0,0 +1,32 @@
+﻿var message = "You have started myScript1";
+var err = function err(errString) {
+    alert(errString);
+    }
+app.onError = err;
+
+alert(message);
+
+app.newProject();
+
+// Make first comp
+
+var compName = "Comp0";
+app.project.items.addComp(compName, 640, 480, 1, 60, 60);
+
+// Grab the first comp
+
+var firstComp = app.project.item(1);
+var layerCollection = firstComp.layers;
+
+// Define colors, other solid parameters
+
+var color = [0.2, 0.3, 0.4];
+var layerName = "Layer0";
+var layerWidth = 640;
+var layerHeight = 480;
+var layerAspect = 1;
+var layerDuration = 60;
+
+// Add Solid Layer
+
+layerCollection.addSolid(color, layerName, layerWidth, layerHeight, layerAspect, layerDuration);
